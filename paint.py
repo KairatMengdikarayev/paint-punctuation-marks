c12099edfeb8abe3e70104c92c17104d2f9defa2
KairatMengdikarayev <54745003+KairatMengdikarayev@users.noreply.github.com>
2020-03-13 09:04:50 +0300

    Add files via upload

---
+++ paint.py
@@ -0,0 +1,38 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from sys import stdout
+from time import sleep
+
+from colorama import init, Fore, Style
+
+
+init()
+print(Fore.GREEN, Style.BRIGHT)
+
+PUNCTUATION_MARKS = (
+	".", 
+	",", 
+	"!", 
+	"?", 
+	"-",
+	"—",
+	":",
+)
+
+JOKE = (
+"""Сорвались в пропасть два альпиниста — оптимист и пессимист. 
+Пессимист: — Падаю... 
+Оптимист: — Лечу!
+"""
+)
+
+for i in JOKE:
+
+	if i in PUNCTUATION_MARKS:
+		print(Fore.RED + i, end="")
+	
+	else:
+		print(Fore.GREEN + i, end="")
+		stdout.flush()
+		sleep(0.1)
