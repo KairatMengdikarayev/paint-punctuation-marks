9f886de7461aadfc6881c0474f07176fa01f166d
Kairat <54745003+KairatMengdikarayev@users.noreply.github.com>
2020-03-27 16:11:01 +0300

    Update paint.py

--- paint.py
+++ paint.py
@@ -8,29 +8,28 @@ init()
 print(Fore.GREEN, Style.BRIGHT)
 
 PUNCTUATION_MARKS = (
-	".", 
-	",", 
-	"!", 
-	"?", 
-	"-",
-	"—",
-	":",
+    ".", ",", "!", "?", 
+    "-", "—", ";", ":",
+    "(", ")", "[", "]",
 )
 
 JOKE = (
-"""Сорвались в пропасть два альпиниста — оптимист и пессимист. 
+"""
+Сорвались в пропасть два альпиниста — оптимист и пессимист. 
 Пессимист: — Падаю... 
 Оптимист: — Лечу!
+
 """
 )
 
 for i in JOKE:
 
-	if i in PUNCTUATION_MARKS:
-		print(Fore.RED + i, end="")
-	
-	else:
-		print(Fore.GREEN + i, end="")
-		
-	stdout.flush()
-	sleep(0.1)
+    if i in PUNCTUATION_MARKS:
+        print(Fore.RED + i, end="")
+    
+    else:
+        print(Fore.GREEN + i, end="")
+
+    stdout.flush()
+    sleep(0.1)
+
